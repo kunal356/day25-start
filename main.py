8f3dd3df2916064a97f2d76368f6036cd6a1db6e
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-01-22 17:46:14 +0000

    229. DataFrames & Series: Working with Rows & Columns

--- main.py
+++ main.py
@@ -20,6 +20,40 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(data)
-print(data["temp"])
+# print(data["temp"])
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# average = data["temp"].mean()
+# print(average)
+#
+# max_value = data["temp"].max()
+# print(max_value)
+
+# Get Data in Column
+# data["temp"]
+# data.temp
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# Convert Monday temperature to Fahrenheit
+# monday_data = data[data.day == "Monday"]
+# temp_to_fahrenheit = monday_data.temp[0] * 9 / 5 + 32
+# print(temp_to_fahrenheit)
+
+# Creating a data Frame
+
+# data_dict = {
+#     "students": ["Jai", "Shree", "Ram"],
+#     "marks": [90, 98, 99]
+# }
+
+# custom_data_frame = pandas.DataFrame(data_dict)
+# print(custom_data_frame)
+
+# Converting Custom Data Frame to CSV data
+# custom_data_frame.to_csv("custom_csv")
